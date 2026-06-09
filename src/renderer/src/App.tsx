272539ef9b14d27faccd6e120ac7f164e8babe44
Naser swei <95959910+naserswei@users.noreply.github.com>
2026-06-09 19:38:38 +0200

    Update app version to 1.0.12

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -53,7 +53,7 @@ function App(): React.JSX.Element {
               </a>
             </Button>
             <div className="rounded-lg border bg-card px-3 py-1.5 text-sm font-medium text-card-foreground">
-              Version 1.0.11
+              Version 1.0.12
             </div>
           </div>
         </header>
